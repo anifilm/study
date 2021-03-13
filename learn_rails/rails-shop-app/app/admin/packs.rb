a986032b87a678616e83c3cfed4c0bf00e534432
anifilm <anifilm02@gmail.com>
2021-03-13 17:08:18 +0900

    study rails-shop-app 2021-03-13

--- learn_rails/rails-shop-app/app/admin/packs.rb
+++ learn_rails/rails-shop-app/app/admin/packs.rb
@@ -15,6 +15,24 @@ ActiveAdmin.register Pack do
   #   permitted
   # end
 
+  index do
+    selectable_column
+
+    id_column
+
+    column :image do |pack|
+      if pack.image.attached?
+        image_tag url_for(pack.image), class: "preview_img"
+      else
+        "이미지 없음"
+      end
+    end
+    column :product_name
+    column :company_name
+
+    actions
+  end
+
   # new, edit 커스텀 부분
   form do |f|
     f.inputs do
@@ -24,6 +42,23 @@ ActiveAdmin.register Pack do
       f.input :desc
     end
     f.actions
+  end
+
+  # show 수정하는 부분
+  show do
+    attributes_table do
+      row :id
+      row :image do |pack|
+        if pack.image.attached?
+          image_tag url_for(pack.image), class: "small_img"
+        else
+          "이미지 없음"
+        end
+      end
+      row :product_name
+      row :company_name
+      row :desc
+    end
   end
 
 end
--- learn_rails/rails-shop-app/config/application.rb
+++ learn_rails/rails-shop-app/config/application.rb
@@ -10,6 +10,8 @@ module RailsShopApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.i18n.default_locale = :ko
+    config.time_zone = "Seoul"
 
     # Configuration for the application, engines, and railties goes here.
     #
