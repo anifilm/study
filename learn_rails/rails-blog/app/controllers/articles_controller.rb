45defb5f4fe16396935eba33707e5becd86303fb
anifilm <anifilm02@gmail.com>
2021-03-13 09:08:24 +0900

    study rails-blog 2021-03-13

--- learn_rails/rails-blog/app/controllers/articles_controller.rb
+++ learn_rails/rails-blog/app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -12,7 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -20,4 +23,30 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body, :status)
+    end
 end
