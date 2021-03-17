5709767151704cc7b7bcdee66cffadd11e6200d3
anifilm <anifilm02@gmail.com>
2021-03-17 19:19:03 +0900

    study rails 2021-03-17

--- learn_rails/clone-beginmate/config/routes.rb
+++ learn_rails/clone-beginmate/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "home#index"
 end
