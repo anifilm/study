2989e499754f41c66e5bd98deeaa07fc4e5ffbb7
anifilm <anifilm02@gmail.com>
2021-03-18 10:38:05 +0900

    자료 추가

---
+++ learn_PHP/php_basic/02-3_var.php
@@ -0,0 +1,17 @@
+<?php
+	$a = 10;
+	$b = 20;
+	$c = $a + $b;
+	echo "$c <br>";
+
+	$a = 18.5;
+	$b = 37.3;
+	$c = $a + $b;
+	echo "$c <br>";
+
+	$fruit = "사과";
+	echo "$fruit <br>";
+
+	$fruit = "오렌지";
+	echo "$fruit <br>";
+?>
