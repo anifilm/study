f0958205fff553f50a95fa6663ec10228bfdfbce
anifilm <anifilm02@gmail.com>
2021-03-18 19:41:31 +0900

    study rails 2021-03-18

--- learn_rails/clone-beginmate/app/controllers/users_controller.rb
+++ learn_rails/clone-beginmate/app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+  def index
+    @users = User.all
+    if params[:region].present?
+      @users = @users.where(region: params[:region])
+    end
+    if params[:role].present?
+      @users = @users.where(role: params[:role])
+    end
+    if params[:career].present?
+      @users = @users.where(career: params[:career])
+    end
+    if params[:status].present?
+      @users = @users.where(status: params[:status])
+    end
   end
 
-  def index
+  def show
+    @user = User.find(params[:id])
   end
 end
--- learn_rails/clone-beginmate/config/routes.rb
+++ learn_rails/clone-beginmate/config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/index'
-  resources :teams, except: [:index]
-  resources :users, only: [:index, :show]
-  #get "users/:id" => "users#show" 생성
-  devise_for :users
-
   root "home#index"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  resources :teams, except: [:index]
+  resources :users, only: [:index, :show]
 end
