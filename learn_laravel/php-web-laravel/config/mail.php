5b8aae25b8e1962abd31e2ba971a5ec853cabde5
anifilm <anifilm02@gmail.com>
2021-03-26 22:31:27 +0900

    study laravel 사용자 인증 2021-03-26

--- learn_laravel/php-web-laravel/config/mail.php
+++ learn_laravel/php-web-laravel/config/mail.php
@@ -84,8 +84,9 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        //'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        //'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => 'johndoe@example.com', 'name' => 'Admin'
     ],
 
     /*
