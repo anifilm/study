97875e80cab8514acb7301a246ac4b48ced8ae3f
anifilm <anifilm02@gmail.com>
2021-03-27 08:23:39 +0900

    Update 2021_03_26_133230_create_articles_table.php

--- learn_laravel/php-web-laravel/database/migrations/2021_03_26_133230_create_articles_table.php
+++ learn_laravel/php-web-laravel/database/migrations/2021_03_26_133230_create_articles_table.php
@@ -4,25 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
-{
+class CreateArticlesTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             //$table->integer('user_id')->unsigned()->index();
-            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned()->index();
             $table->string('title');
             $table->text('content');
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')
-                  ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
@@ -31,8 +28,11 @@ class CreateArticlesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign('articles_user_id_foreign');
+        });
+
         Schema::dropIfExists('articles');
     }
 }
