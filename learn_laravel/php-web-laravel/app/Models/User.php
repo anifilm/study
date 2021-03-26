09a372ad69f306319310896023deb7a951a9ef2a
anifilm <anifilm02@gmail.com>
2021-03-27 08:19:59 +0900

    study laravel 엘로퀀트 ORM 2021-03-27

--- learn_laravel/php-web-laravel/app/Models/User.php
+++ learn_laravel/php-web-laravel/app/Models/User.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     use HasFactory, Notifiable;
 
     /**
@@ -40,4 +39,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function articles() {
+        return $this->hasMany(Article::class);
+    }
 }
---
+++ learn_laravel/php-web-laravel/database/migrations/2021_03_26_133230_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            //$table->integer('user_id')->unsigned()->index();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                  ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
