73d0e8506877ae05126f36cbfba376cdccaaa617
anifilm <anifilm02@gmail.com>
2021-04-07 09:38:16 +0900

    db 수정

--- learn_laravel/codelab-laravel-1st/config/database.php
+++ learn_laravel/codelab-laravel-1st/config/database.php
@@ -38,7 +38,8 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            //'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => __DIR__ . '/../storage/app/db.sqlite',
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
