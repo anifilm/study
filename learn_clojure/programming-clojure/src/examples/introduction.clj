331d3d67774a53150362d3126df2925370af00d0
anifilm <anifilm02@gmail.com>
2021-05-08 16:09:48 +0900

    자료 추가

---
+++ learn_clojure/programming-clojure/src/examples/introduction.clj
@@ -0,0 +1,13 @@
+; (load-file "introduction.clj")
+
+(defn hello
+  "Writes hello message to *out*. Calls you by username.
+  Knows if you have been here before."
+  [username]
+  (dosync
+    (let [past-visitor (@visitors username)]
+      (if past-visitor
+        (str "Welcome back, " username)
+        (do
+          (alter visitors conj username)
+          (str "Hello, " username))))))
---
+++ learn_clojure/programming-clojure/test/programming_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns programming-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [programming-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
