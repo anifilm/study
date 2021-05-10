b277969ba517967b22b0f87be2f13c3ba2f8d0e9
anifilm <anifilm02@gmail.com>
2021-05-11 08:21:09 +0900

    Update exploring.clj

--- learn_clojure/programming-clojure/src/examples/exploring.clj
+++ learn_clojure/programming-clojure/src/examples/exploring.clj
@@ -83,3 +83,12 @@
 ; for loop 매크로, 두 번째
 ;(doseq [i (for [i (range 10)] (inc i))]
 ;  (println "i =" i))
+
+(defn indexed [coll] (map vector (iterate inc 0) coll))
+
+(defn index-filter [pred coll]
+  (when pred
+    (for [[idx elt] (indexed coll) :when (pred elt)] idx)))
+
+(defn index-of-any [pred coll]
+  (first (index-filter pred coll)))
