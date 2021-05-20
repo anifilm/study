199417cf4d2a2da9fe2387b07351234483000cad
anifilm <anifilm02@gmail.com>
2021-05-20 18:02:40 +0900

    Create concatenatable.clj

---
+++ learn_clojure/joy-of-clojure/src/clj/chap01/joy/concatenatable.clj
@@ -0,0 +1,23 @@
+(ns joy.concatenatable)
+
+;;
+;; 예제 1.4
+;;
+(defprotocol Concatenatable
+  (cat [this other]))
+
+(extend-type String
+  Concatenatable
+  (cat [this other]
+    (.concat this other)))
+
+(extend-type java.util.List
+  Concatenatable
+  (cat [this other]
+    (concat this other)))
+
+(cat "House" " of Leaves")
+;;=> "House of Leaves"
+
+(cat [1 2 3] [4 5 6])
+;;=> (1 2 3 4 5 6)
