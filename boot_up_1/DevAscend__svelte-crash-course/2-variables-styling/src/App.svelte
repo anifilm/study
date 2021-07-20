d988fa2ad507cf77e21df60c058119f8db4c7642
anifilm <anifilm02@gmail.com>
2021-07-20 20:53:55 +0900

    study svelte 2021-07-20

---
+++ boot_up_1/DevAscend__svelte-crash-course/2-variables-styling/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+    let name = 'Anifilm';
+    let src = 'favicon.png';
+    let count = 0;
+
+    const incrementCounter = () => {
+        count += 1;
+    };
+</script>
+
+<main>
+    <h1>Hello {name}!</h1>
+    <img {src} alt="Svelte logo" />
+    <h3>Counter: {count}</h3>
+    <button on:click={incrementCounter}>Add 1 to Counter</button>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        font-size: 72px;
+        text-align: center;
+    }
+
+    h3 {
+        font-size: 38px;
+    }
+</style>
