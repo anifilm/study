d5fd5230e072ee26faf9e725703fc951ce857913
anifilm <anifilm02@gmail.com>
2021-07-20 14:12:05 +0900

    study javascript 2021-07-20

--- boot_up_1/dcode__notes-app/js/App.js
+++ boot_up_1/dcode__notes-app/js/App.js
@@ -1,5 +1,5 @@
-import NotesView from "./NotesView.js";
-import NotesAPI from "./NotesAPI.js";
+import NotesView from './NotesView.js';
+import NotesAPI from './NotesAPI.js';
 
 export default class App {
   constructor(root) {
@@ -7,22 +7,58 @@ export default class App {
     this.activeNote = null;
     this.view = new NotesView(root, this._handlers());
 
-    this._refeshNotes();
+    this._refreshNotes();
   }
 
-  _refeshNotes() {
+  _refreshNotes() {
+    const notes = NotesAPI.getAllNotes();
 
+    this._setNotes(notes);
+
+    if (notes.length > 0) {
+      this._setActiveNote(notes[0]);
+    }
   }
 
-  _setNOtes(notes) {
-
+  _setNotes(notes) {
+    this.notes = notes;
+    this.view.updateNoteList(notes);
+    this.view.updateNotePreviewVisibility(notes.length > 0);
   }
 
   _setActiveNote(note) {
-
+    this.activeNote = note;
+    this.view.updateActiveNote(note);
   }
 
-  _handers() {
+  _handlers() {
+    return {
+      onNoteSelect: (noteId) => {
+        const selectedNote = this.notes.find((note) => note.id == noteId);
+        this._setActiveNote(selectedNote);
+      },
+      onNoteAdd: () => {
+        const newNote = {
+          title: 'New Note',
+          body: 'Take note...',
+        };
 
+        NotesAPI.saveNote(newNote);
+        this._refreshNotes();
+      },
+      onNoteEdit: (title, body) => {
+        NotesAPI.saveNote({
+          id: this.activeNote.id,
+          title,
+          body,
+        });
+
+        this._refreshNotes();
+      },
+      onNoteDelete: (noteId) => {
+        NotesAPI.deleteNote(noteId);
+        this._refreshNotes();
+      },
+    };
   }
 }
