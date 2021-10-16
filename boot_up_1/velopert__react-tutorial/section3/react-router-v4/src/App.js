93106401d58188b6789c2e9e20f0ecac0eab5295
anifilm <anifilm02@gmail.com>
2021-10-16 20:42:06 +0900

    study react 2021-10-16

--- boot_up_1/velopert__react-tutorial/section3/react-router-v4/src/App.js
+++ boot_up_1/velopert__react-tutorial/section3/react-router-v4/src/App.js
@@ -3,8 +3,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 
+// route url 구성
 import Home from './routes/Home';
 import About from './routes/About';
+import Posts from './routes/Posts';
+import Login from './routes/Login';
+import MyPage from './routes/MyPage';
 
 const App = () => {
   return (
@@ -12,7 +16,10 @@ const App = () => {
       <div>
         <Header />
         <Route path="/" component={Home} exact />
-        <Route path="/about" component={About} />
+        <Route path="/about/:username" component={About} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/login" component={Login} />
+        <Route path="/me" component={MyPage} />
       </div>
     </Router>
   );
--- boot_up_1/velopert__react-tutorial/section3/react-router-v4/src/components/Header.js
+++ boot_up_1/velopert__react-tutorial/section3/react-router-v4/src/components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './Header.css';
 
 const Header = () => {
   return (
     <div className="header">
-      <Link to="/" className="item">홈</Link>
-      <Link to="/about" className="item">소개</Link>
+      <NavLink to="/" className="item" activeClassName="active" exact>홈</NavLink>
+      <NavLink to="/about/velopert" className="item" activeClassName="active">소개</NavLink>
+      <NavLink to="/posts" className="item" activeClassName="active">포스트</NavLink>
+      <NavLink to="/me" className="item" activeClassName="active">마이페이지</NavLink>
+      <NavLink to="/login" className="item" activeClassName="active">로그인</NavLink>
     </div>
   );
 }
