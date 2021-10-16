3d2bf8bc21549a03542b26ac3ef7d501efa4aeb4
anifilm <anifilm02@gmail.com>
2021-10-16 10:35:14 +0900

    study express 2021-10-16

--- boot_up_1/velopert__react-tutorial/section1/express-tutorial/app.js
+++ boot_up_1/velopert__react-tutorial/section1/express-tutorial/app.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const user = require('./routes/user');
 
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello, World!');
-});
+//const myLogger = function (req, res, next) {
+//  console.log(req.url);
+//  next();
+//};
+
+//app.use(myLogger);
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/', express.static('public'));
+
+//app.get('/', function (req, res) {
+//  res.send('Hello, world!');
+//});
+
+app.use('/user', user);
 
 app.listen(3000, function () {
-  console.log('Example App listening on port 3000');
+  console.log('Example App listening on port 3000\n');
 });
