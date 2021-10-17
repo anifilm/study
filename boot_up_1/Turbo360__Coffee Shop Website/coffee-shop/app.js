c2a0bb1f1f7bf22faf78cfd9db95e13aec103e78
anifilm <anifilm02@gmail.com>
2021-10-17 21:08:12 +0900

    study express 2021-10-17

--- boot_up_1/Turbo360__Coffee Shop Website/coffee-shop/app.js
+++ boot_up_1/Turbo360__Coffee Shop Website/coffee-shop/app.js
@@ -13,7 +13,7 @@ const app = express(); // initialize app
 const config = {
   views: 'views', // Set views directory
   static: 'public', // Set static assets directory
-  logging: true
+  logging: true,
 
   /*  To use the Turbo 360 CMS, from the terminal run
       $ turbo extend cms
@@ -22,6 +22,8 @@ const config = {
   // db: vertex.nedb()
 };
 
+vertex.configureApp(app, config);
+
 // view engine setup
 app.set('view engine', 'html');
 nunjucks.configure(config.views, {
@@ -29,8 +31,6 @@ nunjucks.configure(config.views, {
   express: app, // app 객체 연결
   watch: true,  // HTML 파일이 변경될 때 템플릿 엔진을 다시 렌더링함
 });
-
-vertex.configureApp(app, config);
 
 const main = require('./routes/main');
 app.use('/', main);
--- boot_up_1/Turbo360__Coffee Shop Website/coffee-shop/routes/main.js
+++ boot_up_1/Turbo360__Coffee Shop Website/coffee-shop/routes/main.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const get_home_json = {
+  hero: {
+    header: 'Drink Healthy <br> and Natural Coffee',
+    subtext:
+      "At Joe's House of Coffee, we only brew the freshest coffee from all around the world. We also serve pastries such as cupcakes and brownies.",
+    bg_image: '/images/barista.jpg'
+  },
+  about: {
+    header: "Welcome to Joe's Coffee House.",
+    subtext:
+      'Welcome to our house of coffee! We hope you enjoy our broad selection of premium coffee from all over the world. Welcome to our house of coffee! We hope you enjoy our broad selection of premium coffee from all over the world. Welcome to our house of coffee! We hope you enjoy our broad selection of premium coffee from all over the world.',
+    bg_image: '/images/barista-2.jpg',
+    images: [
+      { image: '/images/coffee.jpg', caption: 'Coffee' },
+      { image: '/images/tea.jpg', caption: 'Tea' },
+      { image: '/images/cupcake.jpg', caption: 'Pastries' }
+    ]
+  }
+};
+
 router.get('/', (req, res, next) => {
-  res.render('home.html', req.context);
+  const data = req.context;
+  data.page = get_home_json;
+  res.render('home.html', data);
 });
 
 router.get('/blog', (req, res, next) => {
