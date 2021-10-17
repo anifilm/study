295ec9925830272b632296adec9a3e9655bfb27f
anifilm <anifilm02@gmail.com>
2021-10-17 17:47:48 +0900

    study express 2021-10-17

--- boot_up_1/Turbo360__Coffee Shop Website/coffee-shop/routes/main.js
+++ boot_up_1/Turbo360__Coffee Shop Website/coffee-shop/routes/main.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const data = {
-    title: "Joe's Coffee House",
-    description: "Welcome to our house of coffee! We hope you enjoy our broad selection of premium coffee from all over the world."
-  };
+  const data = req.context;
   res.render('home.html', data);
+});
+
+router.get('/blog', (req, res, next) => {
+  const data = req.context;
+  res.render('blog.html', data);
 });
 
 module.exports = router;
