57102cda6958491933b1be8dcb17b23711664801
anifilm <anifilm02@gmail.com>
2021-10-21 00:42:24 +0900

    study next.js 2021-10-21

---
+++ boot_up_1/JasonRivera__Build an app with NextJS and MongoDB/notes-app/pages/[id]/edit.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import fetch from 'isomorphic-unfetch';
+import { Button, Form, Loader } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
+
+const EditNote = ({ note }) => {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+EditNote.getInitialProps = async ({ query: { id } }) => {
+  const res = await fetch(`http://localhost:3000/api/notes/${id}`);
+  const { data } = await res.json();
+
+  return { note: data }
+}
+
+export default EditNote;
---
+++ boot_up_1/JasonRivera__Build an app with NextJS and MongoDB/notes-app/pages/[id]/index.js
@@ -0,0 +1,21 @@
+import fetch from 'isomorphic-unfetch';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { Confirm, Button, Loader } from 'semantic-ui-react';
+
+const Note = ({ note }) => {
+  return (
+    <div>
+      note
+    </div>
+  );
+}
+
+Note.getInitialProps = async ({ query: { id } }) => {
+  const res = await fetch(`http://localhost:3000/api/notes/${id}`);
+  const { data } = await res.json();
+
+  return { note: data }
+}
+
+export default Note;
