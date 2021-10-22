061260ada76f3ca8f1c40fa147b07393d4ef35d9
anifilm <anifilm02@gmail.com>
2021-10-23 00:20:37 +0900

    study react 2021-10-23

---
+++ boot_up_1/velopert__react-tutorial/section2/react-tutorial/src/components/PhoneForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class PhoneForm extends Component {
+  state = {
+    name: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="이름" value={this.state.name} onChange={this.handleChange} />
+        <div>{this.state.name}</div>
+      </form>
+    );
+  }
+}
+
+export default PhoneForm;
