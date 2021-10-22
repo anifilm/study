fb206d8ce4dc67a5d27be4b67e2b28dc52e31864
anifilm <anifilm02@gmail.com>
2021-10-23 00:01:29 +0900

    study react 2021-10-23

--- boot_up_1/velopert__react-tutorial/section2/react-tutorial/src/App.js
+++ boot_up_1/velopert__react-tutorial/section2/react-tutorial/src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import Counter from './components/Counter';
+
+import MyComponent from './components/MyComponent';
 
 class App extends Component {
+  state = {
+    counter: 1
+  };
+
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    // 외부 라이브러리 연동: D3, masonry, etc
+    // 컴포넌트에서 필요한 데이터 요청: Ajax, GraphQL, etc
+    // DOM 에 관련된 작업: 스크롤 설정, 크기 읽어오기 등
+    console.log('componentDidMount');
+  }
+
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
   render() {
     return (
-      <>
-        <Counter />
-      </>
+      <div>
+        <h1>안녕하세요. 리액트!</h1>
+        <MyComponent value={this.state.counter} />
+        <button onClick={this.handleClick}>Click Me</button>
+      </div>
     );
   }
 }
