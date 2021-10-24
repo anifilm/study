e788dbac32654d52c95aa4ea991cbadc106883b5
anifilm <anifilm02@gmail.com>
2021-10-24 16:39:10 +0900

    study react 2021-10-24

---
+++ boot_up_1/velopert__react-tutorial/react-codelab-project/react-memopad-v2/server/main.js
@@ -0,0 +1,70 @@
+import express from 'express';
+import path from 'path';
+
+import morgan from 'morgan'; // HTTP REQUEST LOGGER
+import bodyParser from 'body-parser'; // PARSE HTML BODY
+
+import mongoose from 'mongoose';
+import session from 'express-session';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const userId = process.env.USER_ID;
+const userPw = process.env.USER_PW;
+
+/* mongodb connection */
+mongoose
+  .connect(
+    `mongodb+srv://${userId}:${userPw}@cluster0.ehk3h.mongodb.net/MEVNFullStackCRUDApp?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  )
+  .then(() => {
+    console.log('MongoDB connecting Success!!');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+import api from './routes';
+
+const app = express();
+
+const port = process.env.PORT || '4000';
+app.set('port', port);
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+/* use session */
+app.use(session({
+  secret: 'dev',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use('/', express.static(path.join(__dirname, './../public')));
+
+/* setup routers & static directory */
+app.use('/api', api);
+
+app.get('/hello', (req, res) => {
+  return res.send('Hello CodeLab');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './../public/index.html'));
+});
+
+/* handle error */
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}\n`);
+});
