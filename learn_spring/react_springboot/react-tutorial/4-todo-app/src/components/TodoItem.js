12a9db17969a912bd66848c2be42d72806ec1f7a
anifilm <anifilm02@gmail.com>
2021-10-25 16:37:09 +0900

    study react 2021-10-25

--- learn_spring/react_springboot/react-tutorial/4-todo-app/src/components/TodoItem.js
+++ learn_spring/react_springboot/react-tutorial/4-todo-app/src/components/TodoItem.js
@@ -5,8 +5,10 @@ const TodoItem = ({ todo, onToggle, onRemove }) => {
   const { id, text, done } = todo;
   return (
     <div className={styles.item}>
-      <input type="checkbox" checked={done} onChange={() => onToggle(id)} />
-      <span>{text}</span>
+      <label>
+        <input type="checkbox" checked={done} onChange={() => onToggle(id)} />
+        <span>{text}</span>
+      </label>
       <button onClick={() => { onRemove(id) }}>삭제</button>
     </div>
   );
--- learn_spring/react_springboot/react-tutorial/4-todo-app/src/components/Todos.js
+++ learn_spring/react_springboot/react-tutorial/4-todo-app/src/components/Todos.js
@@ -1,11 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoHeader from './TodoHeader';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 import TodoFooter from './TodoFooter';
 
 const Todos = () => {
-  const [todos, setTodos] = useState([
+  const todoItems = [
     {
       id: 1,
       text: 'todoItem1',
@@ -21,59 +21,49 @@ const Todos = () => {
       text: 'todoItem3',
       done: false,
     },
-  ]);
+  ];
+
+  const [todos, setTodos] = useState(todoItems);
 
   // 로컬 변수 정의
   const nextId = useRef(4);
   // Todo 항목 추가 이벤트 처리
-  const onInsert = (text) => {
+  const onInsert = useCallback((text) => {
     const todo = {
       id: nextId.current,
       text: text,
       done: false,
     };
-    //setTodos(todos.concat(todo));
     setTodos((todos) => {
       return todos.concat(todo);
     });
     nextId.current += 1;
-  };
+  }, []);
   // 완료 체크 이벤트 처리
-  const onToggle = (id) => {
-    //setTodos(
-    //  todos.map((todo) => {
-    //    return todo.id === id ? { ...todo, done: !todo.done } : todo;
-    //  }),
-    //);
+  const onToggle = useCallback((id) => {
     setTodos((todos) => {
       return todos.map((todo) => {
         return todo.id === id ? { ...todo, done: !todo.done } : todo;
       });
     });
-  };
+  }, []);
   // Todo 항목 삭제 이벤트 처리
-  const onRemove = (id) => {
-    //setTodos(
-    //  todos.filter((todo) => {
-    //    return todo.id !== id;
-    //  }),
-    //);
+  const onRemove = useCallback((id) => {
     setTodos((todos) => {
       return todos.filter((todo) => {
         return todo.id !== id;
       });
     });
-  };
+  }, []);
   // Todo 완료 항목 모두 삭제 이벤트 처리
-  const onClearAll = () => {
-    //setTodos([]);
+  const onClearAll = useCallback(() => {
     //setTodos(() => { return []; }); // 항목 모두 삭제
     setTodos((todos) => {
       return todos.filter((todo) => {
         return !todo.done;
       });
     });
-  };
+  }, []);
 
   return (
     <div>
