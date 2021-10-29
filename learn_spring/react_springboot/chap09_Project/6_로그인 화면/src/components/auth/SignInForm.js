7af7118d88f8e144ebbe5510603602bf8d0e8a1f
anifilm <anifilm02@gmail.com>
2021-10-29 19:48:30 +0900

    study react 2021-10-29

--- learn_spring/react_springboot/chap09_Project/6_로그인 화면/src/components/auth/SignInForm.js
+++ learn_spring/react_springboot/chap09_Project/6_로그인 화면/src/components/auth/SignInForm.js
@@ -5,28 +5,31 @@ const SignInForm = () => {
   return (
     <div className="container">
       <h3 className="center">로그인</h3>
+      <br />
       <form>
-        <table className="centered">
-          <tbody>
-            <tr>
-              <td>아이디</td>
-              <td>
+        <div className="row">
+          <div className="col s6 offset-s3">
+            <div>
+              <div>아이디</div>
+              <div>
                 <input type="text" />
-              </td>
-            </tr>
-            <tr>
-              <td>비밀번호</td>
-              <td>
+              </div>
+            </div>
+            <div>
+              <div>비밀번호</div>
+              <div>
                 <input type="password" />
-              </td>
-            </tr>
-            <tr>
-              <td colSpan="2">
-                <button type="submit" className="waves-effect waves-light btn">로그인</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <div className="center">
+              <button type="submit" className="waves-effect waves-light btn">로그인</button>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
