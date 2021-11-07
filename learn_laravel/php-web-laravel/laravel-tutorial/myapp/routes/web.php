666b1be9278bb0edbd2438ed6e9e79b61629ada6
anifilm <anifilm02@gmail.com>
2021-11-07 23:09:30 +0900

    study laravel 2021-11-07
    라우팅

--- learn_laravel/php-web-laravel/laravel-tutorial/myapp/routes/web.php
+++ learn_laravel/php-web-laravel/laravel-tutorial/myapp/routes/web.php
@@ -11,6 +11,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//Route::get('/hello', function () {
+//    return '<h1>Hello Foo</h1>';
+//});
+
+//Route::get('/{foo?}', function ($foo='bar') {
+//    return $foo;
+//})->where('foo', '[0-9a-zA-Z]{3}');
+
+Route::get('/', [
+    'as' => 'home',
+    function () {
+        return '제 이름은 "home" 입니다.';
+    }
+]);
+
+Route::get('/home', function () {
+    return redirect(route('home'));
 });
