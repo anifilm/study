3f8291e7b4a4836b387a44050774c2b90fb441a8
anifilm <anifilm02@gmail.com>
2021-11-09 20:07:54 +0900

    study laravel 2021-11-09
    데이터베이스 시딩

--- learn_laravel/php-web-laravel/laravel-tutorial/myapp/database/factories/UserFactory.php
+++ learn_laravel/php-web-laravel/laravel-tutorial/myapp/database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
+use App\Article;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -19,10 +20,20 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name' => $faker->name(),
+        'email' => $faker->unique()->safeEmail(),
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => bcrypt('password'),
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Article::class, function (Faker $faker) {
+    $date = $faker->dateTimeThisMonth();
+    return [
+        'title' => $faker->sentence(),
+        'content' => $faker->paragraph(),
+        'created_at' => $date,
+        'updated_at' => $date,
     ];
 });
