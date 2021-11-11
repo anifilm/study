6291a1a32cf8d7097a2329713835dc6786230d69
anifilm <anifilm02@gmail.com>
2021-11-11 22:18:11 +0900

    study codeignite 2021-11-11

--- learn_PHP/php_codeigniter/codeigniter3_lifecoding/application/controllers/topic.php
+++ learn_PHP/php_codeigniter/codeigniter3_lifecoding/application/controllers/topic.php
@@ -2,8 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Topic extends CI_Controller {
+    public function index() {
+		$this->load->view('topic');
+    }
 
-	public function index() {
-		echo '토픽 페이지';
-	}
+    public function get($id=1) {
+		$data = array('id' => $id);
+        $this->load->view('topic_get', $data);
+    }
 }
---
+++ learn_PHP/php_codeigniter/codeigniter3_lifecoding/application/views/topic.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+</head>
+<body>
+	토픽 메인 페이지 이전
+</body>
+</html>
