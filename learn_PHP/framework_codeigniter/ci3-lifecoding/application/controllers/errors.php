cb2067fd8a82c50e0a9a08273f2f939c29026a19
anifilm <anifilm02@gmail.com>
2021-11-22 11:52:46 +0900

    Delete errors.php

--- learn_PHP/framework_codeigniter/ci3-lifecoding/application/controllers/errors.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Errors extends CI_controller {
-	public function notfound() {
-		$this->load->view('templetes/header');
-		$this->load->view('errors/404');
-		$this->load->view('templetes/footer');
-	}
-}
