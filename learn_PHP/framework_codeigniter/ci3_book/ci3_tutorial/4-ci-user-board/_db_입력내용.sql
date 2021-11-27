12837d3e347cf2dd49665bae87b4083f50f1609e
anifilm <anifilm02@gmail.com>
2021-11-27 13:57:45 +0900

    Update _db_입력내용.sql

--- learn_PHP/framework_codeigniter/ci3_book/ci3_tutorial/4-ci-user-board/_db_입력내용.sql
+++ learn_PHP/framework_codeigniter/ci3_book/ci3_tutorial/4-ci-user-board/_db_입력내용.sql
@@ -2,19 +2,31 @@ CREATE DATABASE php_ci3_book;
 USE php_ci3_book;
 
 
-CREATE TABLE `todo_items` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `content` varchar(200) NULL,
-  `created_on` date NULL,
-  `due_date` date NULL,
-  `use` int(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM COLLATE='utf8_general_ci';
+CREATE TABLE `ci_board` (
+  `board_id` int(10) NOT NULL AUTO_INCREMENT,
+  `board_pid` int(10) NOT NULL DEFAULT '0' COMMENT '원글 번호',
+  `user_id` varchar(20) NOT NULL COMMENT '작성자ID',
+  `user_name` varchar(20) NOT NULL COMMENT '작성자 이름',
+  `subject` varchar(50) NOT NULL COMMENT '게시글 제목',
+  `contents` text NOT NULL COMMENT '게시글 내용',
+  `hits` int(10) NOT NULL DEFAULT '0' COMMENT '조회수',
+  `reg_date` datetime NOT NULL COMMENT '등록일',
+  PRIMARY KEY (`board_id`),
+  INDEX `board_pid` (`board_pid`)
+) ENGINE=MyISAM COLLATE='utf8_general_ci' COMMENT='CodeIgniter 게시판';
 
 
-INSERT INTO `todo_items`
-	(`id`, `content`, `created_on`, `due_date`, `use`)
+INSERT INTO `ci_board`
+	(`board_id`, `board_pid`, `user_id`, `user_name`, `subject`, `contents`, `hits`, `reg_date`)
 VALUES
-	(1, '웅파 미팅', '2012-09-23', '2012-09-24', 1),
-	(2, '스터디', '2012-09-24', '2012-09-25', 1),
-	(3, '테스트', '2012-01-01', '2013-01-01', 1);
+	(1, 0, 'advisor', '웅파', '안녕하세요', '첫글이네요.', 5, '2012-06-12 22:23:01'),
+	(2, 0, 'advisor', '웅파', '두번째 글입니다.', '두번째글이네요.', 0, '2012-06-12 22:24:01'),
+	(3, 0, 'advisor', '웅파', '세번째 글입니다.', '세번째글이네요.', 1, '2012-06-12 22:24:01'),
+	(4, 0, 'advisor', '웅파', '네번째 글입니다.', '네번째글이네요.', 6, '2012-06-12 22:24:01'),
+	(5, 0, 'advisor', '웅파', '다섯번째 글입니다.', '다섯번째글이네요.', 4, '2012-06-12 22:24:01'),
+	(6, 0, 'advisor', '웅파', '여덞번째 글입니다.2', '여덞번째글이네요.2', 13, '2012-06-12 22:24:01'),
+	(7, 0, 'advisor', '웅파', '아홉번째 글입니다.', '아홉번째글이네요.', 1, '2012-06-12 22:24:01'),
+	(8, 0, 'advisor', '웅파', '열번째 글입니다.', '열번째글이네요.', 9, '2012-06-12 22:24:01'),
+	(9, 1, 'blumine', '웅파1', '첫번째 글의 첫번째 댓글입니다.', '첫번째 댓글이네요.', 1, '2012-06-12 22:26:01'),
+	(10, 1, 'blumine', '웅파1', '첫번째 글의 두번째 댓글입니다.', '두번째 댓글이네요.', 0, '2012-06-12 22:27:01'),
+	(11, 2, 'blumine', '웅파1', '두번째 글의 첫번째 댓글입니다.', '두번째 글의 첫번째 댓글이네요.', 9, '2012-06-12 22:29:01'),
