d671268693e4ad4f1ad481c5f4ad9bc242ce537a
anifilm <anifilm02@gmail.com>
2021-11-29 22:03:53 +0900

    study php 2021-11-29

--- learn_PHP/php_mysql_master/chap04/4-2_MySQL-Create/public/index.php
+++ learn_PHP/php_mysql_master/chap04/4-2_MySQL-Create/public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=php_ijdb;charset=utf8', 'ijdbuser', 'mypassword');
+    $pdo = new PDO('mysql:host=localhost;dbname=php_mysql_master_ijdb;charset=utf8', 'ijdbuser', 'mypassword');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $sql = 'CREATE TABLE `joke` (
