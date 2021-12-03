f40b2386e1bf1050bb858d471f5047d488dd0ef5
anifilm <anifilm02@gmail.com>
2021-12-03 09:23:00 +0900

    study jsp 2021-12-03

---
+++ learn_java_web/JSP_Servlet_web_programming/chap03/src/main/java/com/example/chap03/myServlet1.java
@@ -0,0 +1,23 @@
+package com.example.chap03;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(urlPatterns = "/myServlet1")
+public class myServlet1 extends HttpServlet {
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>MyServlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Fighting Korea!!!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
---
+++ learn_java_web/JSP_Servlet_web_programming/chap03/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JSP - Hello World</title>
+</head>
+<body>
+<%
+  String strVar = "Korea Fighting!!!";
+%>
+  JSP 명령어로 작성한 <%= strVar %><br>
+  HTML 태그로 작성한 Korea Fighting!!!
+</body>
+</html>
