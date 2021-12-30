00d9417c71a6f9d1eee4a440aff3c4aef2428897
anifilm <anifilm02@gmail.com>
2021-12-30 13:46:52 +0900

    study django 2021-12-30

--- boot_up_3/AskDjango__beamin_st/beamin-st/baemin/admin.py
+++ boot_up_3/AskDjango__beamin_st/beamin-st/baemin/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Shop, Item, Order
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'tel', 'addr')
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('shop', 'name', 'price')
+    list_filter = ('shop',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('shop', 'user', 'created_at')
+    list_filter = ('shop', 'user')
--- boot_up_3/AskDjango__beamin_st/beamin-st/baemin/models.py
+++ boot_up_3/AskDjango__beamin_st/beamin-st/baemin/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    tel = models.CharField(max_length=20)
+    addr = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    item_set = models.ManyToManyField(Item)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
