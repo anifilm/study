120440c84896ca837ab959a6708fd703cb7560c5
anifilm <anifilm02@gmail.com>
2022-01-07 19:17:10 +0900

    study react 2022-01-07

---
+++ boot_up_2/DailyWebCoding/express-auth-firebase/frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import firebase from 'firebase/app';
+import { useEffect, useState } from 'react';
+
+import ListOfTodo from './components/ListOfTodo';
+
+const App = () => {
+  return (
+    <div className="App">
+      Hello React
+    </div>
+  );
+}
+
+export default App;
---
+++ boot_up_2/DailyWebCoding/express-auth-firebase/frontend/src/components/ListOfTodo.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const ListOfTodo = ({ token }) => {
+  return (
+    <div>
+      <h1>List of todo</h1>
+    </div>
+  );
+}
+
+export default ListOfTodo;
