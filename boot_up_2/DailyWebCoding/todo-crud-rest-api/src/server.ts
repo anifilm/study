c4f4a4df2365d56bdf5b70eef17f3e9bd28b67a8
anifilm <anifilm02@gmail.com>
2022-01-07 13:05:57 +0900

    study node.js 2022-01-07

--- boot_up_2/DailyWebCoding/todo-crud-rest-api/src/server.ts
+++ boot_up_2/DailyWebCoding/todo-crud-rest-api/src/server.ts
@@ -3,7 +3,7 @@ import db from './config/database.config';
 import { v4 as uuidv4 } from 'uuid';
 import { TodoInstance } from './todo/model';
 import TodoValidator from './todo/validator';
-import { validationResult } from 'express-validator';
+import Middleware from './middleware';
 
 db.sync().then(() => {
   console.log('connect to db');
@@ -14,19 +14,9 @@ const port = 8080;
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  return res.send('hello world');
-});
-
 app.post(
   '/create',
-  TodoValidator.checkCreateTodo(), (req: Request, res: Response, next: NextFunction) => {
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-      return res.json(error);
-    }
-    next();
-  },
+  TodoValidator.checkCreateTodo(), Middleware.handleValidationError,
   async (req: Request, res: Response) => {
     //console.log(req.body);
     const id = uuidv4();
@@ -38,6 +28,16 @@ app.post(
     }
   },
 );
+
+app.get('/read', async (req: Request, res: Response) => {
+  try {
+    const records = await TodoInstance.findAll({ where: {} });
+    return res.json(records);
+  }
+  catch (err) {
+    return res.json({ msg: 'fail to read', status: 500, route: '/read' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
