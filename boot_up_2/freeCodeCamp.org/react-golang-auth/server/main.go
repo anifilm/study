5bd69dccdbe2b89a330b95753256f220befa2d17
anifilm <anifilm02@gmail.com>
2022-01-12 19:46:59 +0900

    study golang 2022-01-12

---
+++ boot_up_2/freeCodeCamp.org/react-golang-auth/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	conn, err := gorm.Open(mysql.Open(""), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":3000")
+}
