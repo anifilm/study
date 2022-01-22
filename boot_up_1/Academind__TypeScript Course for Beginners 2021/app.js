43c04858611e0e5678a1efbf873cc4e0464fd93d
anifilm <anifilm02@gmail.com>
2022-01-22 12:36:15 +0900

    study typescript 2022-01-22

--- boot_up_1/Academind__TypeScript Course for Beginners 2021/app.js
+++ boot_up_1/Academind__TypeScript Course for Beginners 2021/app.js
@@ -1,1 +1,17 @@
-console.log('Your code goes here...');
+function add(n1, n2, showResult, phrase) {
+    //if (typeof n1 !== 'number' && typeof n2 !== 'number') {
+    //  throw new Error('Incorrect Input!');
+    //}
+    var result = n1 + n2;
+    if (showResult === true) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5; // 5.8
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
