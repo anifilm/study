8640c3f73abf710d39170d509291e4b21c1f8dd5
anifilm <anifilm02@gmail.com>
2022-02-05 11:15:01 +0900

    study aws-serverless 2022-02-05

---
+++ aws-serverless/full-stack-serverless/study-serverless/amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp990946f2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
