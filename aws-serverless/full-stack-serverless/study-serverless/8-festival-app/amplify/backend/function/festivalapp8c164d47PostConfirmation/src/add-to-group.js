8c17393de77105375abfc5cd701e73955e855e98
anifilm <anifilm02@gmail.com>
2022-02-07 21:58:44 +0900

    Update add-to-group.js

--- aws-serverless/full-stack-serverless/study-serverless/8-festival-app/amplify/backend/function/festivalapp8c164d47PostConfirmation/src/add-to-group.js
+++ aws-serverless/full-stack-serverless/study-serverless/8-festival-app/amplify/backend/function/festivalapp8c164d47PostConfirmation/src/add-to-group.js
@@ -1,35 +1,44 @@
 const aws = require('aws-sdk');
 
-const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({
+const cognitoProvider = new aws.CognitoIdentityServiceProvider({
   apiVersion: '2016-04-18',
 });
 
-exports.handler = async event => {
-  const groupParams = {
-    GroupName: process.env.GROUP,
-    UserPoolId: event.userPoolId,
-  };
-  const addUserParams = {
-    GroupName: process.env.GROUP,
-    UserPoolId: event.userPoolId,
-    Username: event.userName,
-  };
-
-  try {
-    await cognitoidentityserviceprovider.getGroup(groupParams).promise();
-  } catch (e) {
-    await cognitoidentityserviceprovider.createGroup(groupParams).promise();
-  }
-
+exports.handler = async (event, context, callback) => {
   let isAdmin = false;
   const adminEmails = ['anifilm02@gmail.com'];
+
+  // 사용자가 관리자 중 한 명이라면 isAdmin 변수를 true로 설정
   if (adminEmails.indexOf(event.request.userAttributes.email) !== -1) {
     isAdmin = true;
   }
-  // 사용자가 관리자인 경우 그룹에 추가
-  if (isAdmin) {
-    await cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams).promise();
-  }
 
-  return event;
+  if (isAdmin) {
+    const groupParams = {
+      UserPoolId: event.userPoolId,
+      GroupName: 'Admin',
+    };
+    const userParams = {
+      UserPoolId: event.userPoolId,
+      Username: event.userName,
+      GroupName: 'Admin',
+    };
+
+    // 그룹이 있는지 확인하고, 없으면 그룹을 생성
+    try {
+      await cognitoProvider.getGroup(groupParams).promise();
+    } catch (e) {
+      await cognitoProvider.createGroup(groupParams).promise();
+    }
+    // 사용자가 관리자라면 Admin 그룹에 추가
+    try {
+      await cognitoProvider.adminAddUserToGroup(userParams).promise();
+      callback(null, event);
+    } catch (e) {
+      callback(e);
+    }
+  } else {
+    // 사용자가 어느 그룹에도 속하지 않으면 아무 작업도 하지 않음
+    callback(null, event);
+  }
 };
