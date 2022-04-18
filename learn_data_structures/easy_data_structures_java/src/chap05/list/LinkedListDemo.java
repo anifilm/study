290e93ca1694729895a96842affaf0f59e6d4ba1
anifilm <anifilm02@gmail.com>
2022-04-18 15:12:39 +0900

    study data structures 2022-04-18

---
+++ learn_data_structures/easy_data_structures_java/src/chap05/list/LinkedListDemo.java
@@ -0,0 +1,32 @@
+package chap05.list;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+
+        System.out.println("Linked List Demo!");
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(0, 300); // 오토 박싱
+        list.add(0, 200);
+        list.add(0, 100);
+        list.printAll();
+
+        list.append(500);
+        list.append(600);
+        list.printAll();
+
+        list.remove(3);
+        list.printAll();
+
+        list.add(3, 250);
+        list.add(1, 50);
+        list.add(0, 10);
+        list.append(700);
+        list.printAll();
+
+        list.remove(1);
+        list.removeItem(600);
+        list.printAll();
+    }
+}
