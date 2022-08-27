50b31fcd517af8c68004920b91c647455bbfce1b
anifilm <anifilm02@gmail.com>
2022-08-27 19:07:35 +0900

    study algorithm 2022-08-27

---
+++ learn_data_structures/This_is_data_structure_algorithm_with_C/chap02/Calculator/Calculator.h
@@ -0,0 +1,25 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <stdlib.h>
+#include "LinkedListStack.h"
+
+typedef enum {
+    LEFT_PARENTHESIS = '(',
+    RIGHT_PARENTHESIS = ')',
+    PLUS = '+',
+    MINUS = '-',
+    MULTIPLY = '*',
+    DIVIDE = '/',
+    SPACE = ' ',
+    OPERAND
+} SYMBOL;
+
+int IsNumber(char Cipher);
+unsigned int GetNextToken(char* Expression, char* Token, int* TYPE);
+int GetPriority(char Operator, int InStack);
+int IsPrior(char OperatorInStack, char OperatorInToken);
+void GetPostfix(char* InfixExpression, char* PostfixExpression);
+double Calculate(char* PostfixExpression);
+
+#endif
