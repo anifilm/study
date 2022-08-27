2abf3180f6301c6e35c45bb6194f0ff9393eb6c8
anifilm <anifilm02@gmail.com>
2022-08-27 17:05:54 +0900

    study algorithm 2022-08-27

---
+++ learn_data_structures/This_is_data_structure_algorithm_with_C/chap02/LinkedListStack/LinkedListStack.h
@@ -0,0 +1,31 @@
+#ifndef LINKEDLIST_STACK_H
+#define LINKEDLIST_STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct tagNode {
+    char* Data;
+    struct tagNode* NextNode;
+} Node;
+
+typedef struct tagLinkedListStack {
+    Node* List;
+    Node* Top;
+} LinkedListStack;
+
+void LLS_CreateStack(LinkedListStack** Stack);
+void LLS_DestroyStack(LinkedListStack* Stack);
+
+Node* LLS_CreateNode(char* NewData);
+void LLS_DestroyNode(Node* _Node);
+
+void LLS_Push(LinkedListStack* Stack, Node* NewNode);
+Node* LLS_Pop(LinkedListStack* Stack);
+
+Node* LLS_Top(LinkedListStack* Stack);
+int LLS_GetSize(LinkedListStack* Stack);
+int LLS_IsEmpty(LinkedListStack* Stack);
+
+#endif
