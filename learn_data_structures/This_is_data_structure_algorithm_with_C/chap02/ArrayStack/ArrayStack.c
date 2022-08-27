151b7322f4bbfb1bb099152c410ab0c090dff61c
anifilm <anifilm02@gmail.com>
2022-08-27 16:30:27 +0900

    자료 이동

---
+++ learn_data_structures/This_is_data_structure_algorithm_with_C/chap02/ArrayStack/ArrayStack.c
@@ -0,0 +1,42 @@
+#include "ArrayStack.h"
+
+void AS_CreateStack(ArrayStack** Stack, int Capacity) {
+    (*Stack) = (ArrayStack*)malloc(sizeof(ArrayStack));
+    (*Stack)->Nodes = (Node*)malloc(sizeof(Node) * Capacity);
+
+    (*Stack)->Capacity = Capacity;
+    (*Stack)->Top = -1;
+}
+
+void AS_DestroyStack(ArrayStack* Stack) {
+    free(Stack->Nodes);
+    free(Stack);
+}
+
+void AS_Push(ArrayStack* Stack, ElementType Data) {
+    Stack->Top++;
+    Stack->Nodes[Stack->Top].Data = Data;
+}
+
+ElementType AS_Pop(ArrayStack* Stack) {
+    int Position = Stack->Top--;
+
+    return Stack->Nodes[Position].Data;
+}
+
+ElementType AS_Top(ArrayStack* Stack) {
+    return Stack->Nodes[Stack->Top].Data;
+}
+
+int AS_GetSize(ArrayStack* Stack) {
+    return Stack->Top + 1;
+}
+
+int AS_IsEmpty(ArrayStack* Stack) {
+    return (Stack->Top == -1);
+}
+
+// VITAMIN QUIZ 2-2
+int AS_IsFull(ArrayStack* Stack) {
+    return (Stack->Top == Stack->Capacity);
+}
---
+++ learn_data_structures/This_is_data_structure_algorithm_with_C/chap02/ArrayStack/Test_ArrayStack.c
@@ -0,0 +1,58 @@
+#include "ArrayStack.h"
+
+int main() {
+    int i = 0;
+    ArrayStack* Stack = NULL;
+
+    AS_CreateStack(&Stack, 10);
+
+    AS_Push(Stack, 3);
+    AS_Push(Stack, 37);
+    AS_Push(Stack, 11);
+    AS_Push(Stack, 12);
+
+    printf("Capacity: %d, Size: %d, Top: %d\n\n", Stack->Capacity, AS_GetSize(Stack), AS_Top(Stack));
+
+    for (i = 0; i < 4; i++) {
+        if (AS_IsEmpty(Stack)) break;
+
+        printf("Popped: %d, ", AS_Pop(Stack));
+
+        if (!AS_IsEmpty(Stack)) {
+            printf("Current Top: %d\n", AS_Top(Stack));
+        }
+        else {
+            printf("Stack is Empty.\n");
+        }
+    }
+
+    // VITAMIN QUIZ 2-2
+    printf("\n");
+    for (i = 1; i < 15; i++) {
+        if (AS_IsFull(Stack)) {
+            printf("Stack is Full.\n");
+            break;
+        }
+
+        printf("Pushed: %d, ", i); AS_Push(Stack, i);
+        printf("Current Top: %d\n", AS_Top(Stack));
+    }
+
+    printf("\n");
+    for (i = 0; i < 15; i++) {
+        if (AS_IsEmpty(Stack)) break;
+
+        printf("Popped: %d, ", AS_Pop(Stack));
+
+        if (!AS_IsEmpty(Stack)) {
+            printf("Current Top: %d\n", AS_Top(Stack));
+        }
+        else {
+            printf("Stack is Empty.\n");
+        }
+    }
+
+    AS_DestroyStack(Stack);
+
+    return 0;
+}
