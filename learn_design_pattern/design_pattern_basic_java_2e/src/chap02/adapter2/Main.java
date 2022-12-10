515ac78fb751cdd95fa6080386e9299ff3607988
anifilm <anifilm02@gmail.com>
2022-12-10 16:30:19 +0900

    studu java 2022-12-10

--- learn_design_pattern/design_pattern_basic_java_2e/src/chap02/adapter2/Main.java
+++ learn_design_pattern/design_pattern_basic_java_2e/src/chap02/adapter2/Main.java
@@ -2,6 +2,8 @@ package chap02.adapter2;
 
 public class Main {
     public static void main(String[] args) {
-
+        Print p = new PrintBanner("Hello");
+        p.printWeak();
+        p.printStrong();
     }
 }
---
+++ learn_design_pattern/design_pattern_basic_java_2e/src/chap02/adapter2/Print.java
@@ -0,0 +1,6 @@
+package chap02.adapter2;
+
+public abstract class Print {
+    public abstract void printWeak();
+    public abstract void printStrong();
+}
