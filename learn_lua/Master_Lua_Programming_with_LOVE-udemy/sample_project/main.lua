363e0fcc9e734436f147b9f0e937b1a37695a8e9
임채영 <anifilm@katkit.co.kr>
2024-03-20 17:28:47 +0900

    study lua 2024-03-20

--- learn_lua/Master_Lua_Programming_with_LOVE-udemy/sample_project/main.lua
+++ learn_lua/Master_Lua_Programming_with_LOVE-udemy/sample_project/main.lua
@@ -1,3 +1,15 @@
+local message = "lua is awesome!!!"
+local condition = -25
+
+if condition > 0 then
+    message = "greater 0"
+elseif condition < 0 then
+    message = "less 0"
+else
+    message = "zero"
+end
+
 function love.draw()
-    love.graphics.print("Hello, world!")
+    love.graphics.setFont(love.graphics.newFont(50))
+    love.graphics.print(message)
 end
